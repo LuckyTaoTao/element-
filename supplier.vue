b278e9f5465f75d1acb313aad01ef8ea4f9946dd
LuckyTaoTao <34666627+LuckyTaoTao@users.noreply.github.com>
2018-05-23 10:04:39 +0800

    Update supplier.vue

--- supplier.vue
+++ supplier.vue
@@ -174,12 +174,9 @@
 </template>
 
 <script>
-import { regionData } from "element-china-area-data";
 export default {
   data() {
     return {
-      options: regionData,
-      selectedOptions: [],
       TableData: {
         tableData: [],
         per_page: [10],
